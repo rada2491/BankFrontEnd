6aa05bc63080585c99d3df311a8fbc836bff4e8b
Carlos <cbr2491@gmail.com>
2018-08-27 16:50:47 -0600

    wip

--- src/redux/actionCreator/loginCredentials.js
+++ src/redux/actionCreator/loginCredentials.js
@@ -3,14 +3,21 @@ import * as a from '../actions/types'
 const API = 'https://localhost:44318/api/autho/login';
 const API2 = 'https://localhost:44318/api/admin';
 const API3 = 'https://localhost:44318/api/client/';
+const API4 = 'https://localhost:44318/api/account/transaction';
+let token, autho, email, id, name;
+
+/* Test transaction */
+let tran = {
+  'origin': '7946138520',
+  'destiny': '79461385',
+  'mount': 25000000000000000
+}
 
 export default function loginCredentials(credentials) {
   return async dispatch => {
     dispatch({
       type: a.LOGIN_REQUEST
     })
-    let token;
-    let autho;
     try {
       await fetch(API, {
         method: 'POST',
@@ -22,10 +29,36 @@ export default function loginCredentials(credentials) {
         .then(body => {
           token = body.token
           autho = body.authorization
+          email = body.userEmail
+          id = body.id
+          name = body.userName
         })
         .then(success => console.log('Success:', success))
         .catch(error => console.log('Error:', error))
-      if (autho === 'Admin') {
+
+      sessionStorage.setItem("token", token)
+      sessionStorage.setItem("userId", id)
+      sessionStorage.setItem("userName", name)
+      sessionStorage.setItem("userEmail", email)
+      sessionStorage.setItem("Authorization", autho)
+      console.log(sessionStorage.getItem("token"))
+      console.log(sessionStorage.getItem("userId"))
+      console.log(sessionStorage.getItem("userName"))
+      console.log(sessionStorage.getItem("userEmail"))
+      console.log(sessionStorage.getItem("Authorization"))
+
+      console.log(tran)
+      await fetch(API4, {
+        method: 'POST',
+        body: JSON.stringify(tran),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+        }
+      }).then(success => console.log('Success:', success))
+        .catch(error => console.log('Error:', error))
+
+      if (sessionStorage.getItem("Authorization") === "Admin") {
         const myHeaders = new Headers({
           'Content-Type': 'application/json',
           'Authorization': 'Bearer ' + token
@@ -47,7 +80,7 @@ export default function loginCredentials(credentials) {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
       });
-      const response2 = await fetch(API3+autho, {
+      const response2 = await fetch(API3 + autho, {
         method: 'GET',
         headers: myHeaders
       })
